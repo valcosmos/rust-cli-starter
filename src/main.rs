dcc9a72a207062d3928dba0fb54006277ab4f6bf
Cupid Valentine <westerngod@126.com>
2024-04-30 23:46:57 +0800

    refactor: optimized code

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,33 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 
-use anyhow;
 use clap::Parser;
-use csv::Reader;
-use serde::{Deserialize, Serialize};
-use std::{fs, path::Path};
+use rcli::{process_csv, Opts, SubCommand};
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    #[serde(rename = "Name")]
-    name: String,
-
-    #[serde(rename = "Position")]
-    position: String,
-
-    #[serde(rename = "DOB")]
-    dob: String,
-
-    #[serde(rename = "Nationality")]
-    nationality: String,
-
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
-
-#[derive(Debug, Parser)]
-#[command(name="rcli", version, author, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
-    Csv(CsvOpts),
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser=verify_input_file)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
 
     match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let mut reader = Reader::from_path(opts.input)?;
-            let mut ret = Vec::with_capacity(128);
-            // let records = reader
-            //     .deserialize()
-            //     .map(|record| record.unwrap())
-            //     .collect::<Vec<Player>>();
-            // println!("{:?}", records);
-            for result in reader.deserialize() {
-                let record: Player = result?;
-                println!("{:?}", record);
-                ret.push(record);
-            }
-            let json  = serde_json::to_string_pretty(&ret)?;
-            fs::write(opts.output, json)?;
-        }
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+            // let mut reader = Reader::from_path(opts.input)?;
+            // let mut ret = Vec::with_capacity(128);
+            // // let records = reader
+            // //     .deserialize()
+            // //     .map(|record| record.unwrap())
+            // //     .collect::<Vec<Player>>();
+            // // println!("{:?}", records);
+            // for result in reader.deserialize() {
+            //     let record: Player = result?;
+            //     println!("{:?}", record);
+            //     ret.push(record);
+            // }
+            // let json  = serde_json::to_string_pretty(&ret)?;
+            // fs::write(opts.output, json)?;
+        
     }
 
     Ok(())
 
     // println!("{:?}", opts);
-}
-
-fn verify_input_file(filename: &str) -> Result<String, &'static str> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())    
-    } else {
-        Err("File does not exist")
-    }
 }
 
