ca3b849f4dc34623402c1847d05b0bb5a460cfaf
Cupid Valentine <westerngod@126.com>
2024-04-30 23:24:12 +0800

    feat: completed convert csv to json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+// rcli csv -i input.csv -o output.json --header -d ','
+
+use anyhow;
+use clap::Parser;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    #[serde(rename = "Name")]
+    name: String,
+
+    #[serde(rename = "Position")]
+    position: String,
+
+    #[serde(rename = "DOB")]
+    dob: String,
+
+    #[serde(rename = "Nationality")]
+    nationality: String,
+
+    #[serde(rename = "Kit Number")]
+    kit: u8,
 }
+
+#[derive(Debug, Parser)]
+#[command(name="rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser=verify_input_file)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            let mut reader = Reader::from_path(opts.input)?;
+            let mut ret = Vec::with_capacity(128);
+            // let records = reader
+            //     .deserialize()
+            //     .map(|record| record.unwrap())
+            //     .collect::<Vec<Player>>();
+            // println!("{:?}", records);
+            for result in reader.deserialize() {
+                let record: Player = result?;
+                println!("{:?}", record);
+                ret.push(record);
+            }
+            let json  = serde_json::to_string_pretty(&ret)?;
+            fs::write(opts.output, json)?;
+        }
+    }
+
+    Ok(())
+
+    // println!("{:?}", opts);
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())    
+    } else {
+        Err("File does not exist")
+    }
+}
+
